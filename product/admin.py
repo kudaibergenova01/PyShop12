0b6f1ec2fcc7250009cda15c3d6ce8a2b32e0e35
kudaibergenova01 <kudaibergenovab01@gmail.com>
2021-07-27 14:36:03 +0600

    Initial commit.

---
+++ product/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Product, Category
+
+admin.site.register(Category)
+admin.site.register(Product)
+
+#TODO: создать категории Smartphones(Samsung, Iphone, Xiaomi),
+# Notebooks(MacBook, ASUS, Acer), Earphones
